46b68d7d72365942418674fdcf40b1935b764339
Kalthoum AL-Atrash <kalthoum_4853@limu.edu.ly>
2026-03-30 12:35:50 +0200

    feat: set up local storage service and shared preferences implementation

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,7 +1,9 @@
+import 'package:exercise_app/core/services/local_storage_service.dart';
+import 'package:exercise_app/core/services/shared_preferences_service.dart';
 import 'package:get_it/get_it.dart';
 
 final locater = GetIt.instance;
 
 Future <void> init() async{
-
+  locater.registerLazySingleton<LocalStorageService>(() => SharedPreferencesService());
 }
