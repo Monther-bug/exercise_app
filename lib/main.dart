431010b46ce6b9315341dc43d239e44edf1c35ea
Kalthoum AL-Atrash <kalthoum_4853@limu.edu.ly>
2026-04-01 12:04:05 +0200

    firebase configured | set up auth structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:exercise_app/core/di/injection_container.dart' as di;
 import 'package:exercise_app/my_app.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await di.init();
   runApp(const MyApp());
 }
